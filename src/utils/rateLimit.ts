2f9cfbf5cb5fcd9066fbb6c82e09b56dbed2edef
Jeffrey Rose <jtr219@outlook.com>
2021-04-30 23:46:31 -0400

    set up redis for rate limiting

---
+++ src/utils/rateLimit.ts
@@ -0,0 +1,32 @@
+import { MiddlewareFn } from 'type-graphql'
+import Redis from 'ioredis'
+import { MyContext } from './myContext'
+
+const redis = new Redis()
+const ONE_DAY = 60 * 60 * 24
+const standardLimit = 500
+
+type RateLimit = (limit?: number) => MiddlewareFn<MyContext>
+export const rateLimit: RateLimit = (limit = standardLimit) => async (
+  { context: { req } /*, info */ },
+  next
+) => {
+  // for now, a simple daily user rate limit is sufficient
+  // if we need to rate limit specific queries
+  // we can use the following commented out code
+  //const key = `rate-limit:${info.fieldName}:${req.ip}`
+  const key = `rate-limit:${req.ip}`
+
+  const current = await redis.incr(key)
+  console.log(key, ' is at ', current)
+  if (current > limit) {
+    throw new Error(
+      `You have exceeded the daily rate limit of ${limit} to the server. Please wait until the following day.`
+    )
+  } else if (current === 1) {
+    console.log('expired')
+    await redis.expire(key, ONE_DAY)
+  }
+
+  return next()
+}
