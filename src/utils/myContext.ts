d38519cbf09f40ff991c24f212d7d987d1eeb35b
Jeffrey Rose <jtr219@outlook.com>
2021-05-02 01:22:59 -0400

    add isAuth middleware

--- src/utils/myContext.ts
+++ src/utils/myContext.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express'
 import { Redis } from 'ioredis'
+import { Session } from 'express-session'
 
 export type MyContext = {
-  req: Request
+  req: Request & { session: IGetUserIDSession }
   res: Response
   redis: Redis
+}
+
+interface IGetUserIDSession extends Session {
+  userId?: number
 }
