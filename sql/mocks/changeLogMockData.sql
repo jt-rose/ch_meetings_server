c008e764de7c7ae3cbb6748e4982ceea709c3e5c
Jeffrey Rose <jtr219@outlook.com>
2021-05-10 01:21:47 -0400

    add mock data for change log

---
+++ sql/mocks/changeLogMockData.sql
@@ -0,0 +1,50 @@
+-- log_id BIGSERIAL PRIMARY KEY,
+--workshop BIGINT REFERENCES workshops (workshop_id) NOT NULL,
+--note TEXT NOT NULL,
+--log_date TIMESTAMPTZ NOT NULL
+INSERT INTO change_log (workshop, note, log_date)
+VALUES (
+        1,
+        'workshop request created',
+        '2020-11-15T11:23:00EST'
+    ),
+    (1, 'workshop assigned', '2020-11-17T15:44:00EST'),
+    (
+        2,
+        'workshop request created',
+        '2021-01-22T12:18:00EST'
+    ),
+    (2, 'workshop assigned', '2021-01-29T16:02:00EST'),
+    (
+        3,
+        'workshop request created',
+        '2021-02-03T09:01:00EST'
+    ),
+    (3, 'workshop assigned', '2021-02-10T12:56:00EST'),
+    (
+        4,
+        'workshop request created',
+        '2021-11-01T10:03:00EST'
+    ),
+    (4, 'workshop assigned', '2021-11-17T15:34:00EST'),
+    (
+        4,
+        'session 3 timing conflict for client. Placed in Hold while gathering new dates',
+        '2021-11-18T07:34:00EST'
+    ),
+    (
+        5,
+        'workshop request created',
+        '2020-12-20T08:24:00EST'
+    ),
+    (5, 'workshop assigned', '2020-12-22T15:54:00EST'),
+    (
+        6,
+        'workshop request created',
+        '2021-03-24T09:39:00EST'
+    ),
+    (
+        6,
+        'session 1 and 4 confirmed, waiting for confirmation on 2 and 3',
+        '2021-03-28T16:47:00EST'
+    )
