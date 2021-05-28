a6876f041df8ce599ba76fc0b1d98fe5a2609052
Jeffrey Rose <jtr219@outlook.com>
2021-05-28 17:24:14 -0400

    add sql script for clearing schema

---
+++ sql/clearSchema.sql
@@ -0,0 +1,7 @@
+-- script for ending background processes and clearing the schema
+-- used when resetting schema
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'ch_meetings'
+    AND pid <> pg_backend_pid();
+DROP SCHEMA "public" CASCADE;
