ed96934dd0f0dc85ef5bcad9809840196a717be2
Jeffrey Rose <jtr219@outlook.com>
2021-05-28 00:00:16 -0400

    add table for session date requests

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -72,12 +72,29 @@ CREATE TABLE workshops (
 CREATE TABLE workshop_sessions (
     workshop_session_id SERIAL PRIMARY KEY,
     workshop_id INT REFERENCES workshops (workshop_id) NOT NULL,
-    date_and_time TIMESTAMPTZ NOT NULL,
+    date_and_time TIMESTAMPTZ,
+    -- date and time are set to nullable
+    -- to accomodate requests where the date is not set yet
+    -- server or DB validation will be used
+    -- to mandate a date_and_time value
+    -- when session_status has moved out of 'REQUESTED'
     session_status SESSION_STATUS_ENUM NOT NULL,
     duration_in_hours DECIMAL(2, 1) NOT NULL,
     zoom_link VARCHAR(255)
 );
--- currently, sessions are set up around specific dates
+-- time ranges for session requests can be entered
+-- time range will default on server to cover a 24 hour period
+-- of each selected date if not specified by user
+-- if a gap occurs (ex: 9-11 or 3-5)
+-- these will be registered as separate session_requests
+-- this allows users to request sessions with a large range of dates/ times
+-- rather than be locked into a specific date when requesting
+CREATE TABLE session_date_and_time_requests (
+    request_id SERIAL PRIMARY KEY,
+    workshop_session_id INT REFERENCES workshop_sessions (workshop_session_id) NOT NULL,
+    start_of_time_range TIMESTAMPTZ NOT NULL,
+    end_of_time_range TIMESTAMPTZ NOT NULL
+) -- currently, sessions are set up around specific dates
 -- with date ranges left for associated session notes
 -- this may be changed later to more explicitly recognize date ranges
 CREATE TABLE change_log (
@@ -93,12 +110,12 @@ CREATE TABLE manager_assignments (
     active BOOLEAN NOT NULL
 );
 CREATE TABLE workshop_notes (
-    notes_id SERIAL PRIMARY KEY,
+    note_id SERIAL PRIMARY KEY,
     workshop_id INT REFERENCES workshops (workshop_id) NOT NULL,
     note TEXT NOT NULL
 );
 CREATE TABLE session_notes (
-    notes_id SERIAL PRIMARY KEY,
+    note_id SERIAL PRIMARY KEY,
     workshop_session_id INT REFERENCES workshop_sessions (workshop_session_id) NOT NULL,
     note TEXT NOT NULL
 )
