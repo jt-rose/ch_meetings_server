d26d5273ac36c5a6004a808c9aa81a11bb73643e
Jeffrey Rose <jtr219@outlook.com>
2021-05-31 23:54:48 -0400

    set up docker for redis - local env

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,10 +1,11 @@
 import Redis from 'ioredis'
 
-console.log(process.env.REDIS_HOST)
+const host = process.env.REDIS_HOST || 'localhost'
+console.log('redis host:', host)
 export const redis = new Redis({
   // overrides local host when in testing env
   // REDIS_HOST env variable will be provided directly by docker
-  host: process.env.REDIS_HOST || 'localhost',
+  host,
   // default port of 6379 used for local host and docker testing env
   port: 6379,
 })
