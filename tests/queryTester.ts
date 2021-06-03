5609325c9b8134e34fe2172fa36c71c5009c9095
Jeffrey Rose <jtr219@outlook.com>
2021-06-03 12:02:15 -0400

    add courses resolver testing

--- tests/queryTester.ts
+++ tests/queryTester.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8378/graphql'
+const API_URL = `http://localhost:5000/graphql`
 
 // run a graphql query against the testing environment
 export const testQuery = async (graphqlRequest: string, variables?: any) =>
--- tests/resolvers/courses.test.ts
+++ tests/resolvers/courses.test.ts
@@ -4,26 +4,29 @@ import { testQuery } from '../queryTester'
 import { seed } from '../../prisma/seed'
 import { clear } from '../../prisma/clear'
 
-before('clear any data at the start', async () => {
-  await clear()
-})
+describe('Course Resolvers', function () {
+  /* --------------------- seed and clear DB for each test -------------------- */
 
-beforeEach('seed database', async () => {
-  await seed()
-})
+  before('clear any data at the start', async function () {
+    await clear()
+  })
 
-afterEach('clear database', async () => {
-  await clear()
-})
-
-after('restore database for local testing', async () => {
+  beforeEach('seed database', async function () {
     await seed()
   })
-  
-describe('Course Resolvers', () => {
-    /* ------------------------------ create course ----------------------------- */
-    it('create course', async () => {
-        const result = await testQuery(`#graphql
+
+  afterEach('clear database', async function () {
+    await clear()
+  })
+
+  after('restore database for local testing', async function () {
+    await seed()
+  })
+
+  /* ------------------------------ create course ----------------------------- */
+
+  it('create course', async function () {
+    const result = await testQuery(`#graphql
          mutation {
             addCourse(courseData: { course_name: "test_course", course_description: "course_description", active: true, virtual_course: true}) {
               course_name
@@ -35,26 +38,22 @@ describe('Course Resolvers', () => {
       `)
 
     const expectedResult = {
-        "data": {
-            "addCourse": {
-              "course_name": "test_course",
-              "course_description": "course_description",
-              "active": true,
-              "virtual_course": true
-            }
-          }
+      data: {
+        addCourse: {
+          course_name: 'test_course',
+          course_description: 'course_description',
+          active: true,
+          virtual_course: true,
+        },
+      },
     }
     expect(result.data).to.eql(expectedResult)
-    })
-/* ---------------- reject creating course -> already exisits --------------- */
-    it('reject creating course when course name already exists', async () => {
-        expect(true).to.be.false
-    })
-
-    it('retrieve course', async () => {
-        const result = await testQuery(`#graphql
-        query {
-  getCourse(course_name: "Course 101") {
+  })
+  /* ---------------- reject creating course -> already exisits --------------- */
+  it('reject creating course when course name already exists', async function () {
+    const result = await testQuery(`#graphql
+        mutation {
+  addCourse(courseData: { course_name: "Course 301", course_description: "course_description", active: true, virtual_course: true}) {
     course_name
     course_description
     active
@@ -63,13 +62,41 @@ describe('Course Resolvers', () => {
 }
         `)
 
-        const expectedResult = ''
+    expect(
+      result.data.errors[0].message.includes(
+        'Unique constraint failed on the fields: (`course_name`)'
+      )
+    ).to.be.true
+  })
 
-        expect(result.data).to.eql(expectedResult)
-    })
-    /* -------------------------- retrieve all courses -------------------------- */
-    it('retrieve all courses', async () => {
-        const result = await testQuery(`#graphql
+  it('retrieve course', async function () {
+    const result = await testQuery(`#graphql
+        query {
+  getCourse(course_name: "Course 301") {
+    course_name
+    course_description
+    active
+    virtual_course
+  }
+}
+        `)
+
+    const expectedResult = {
+      data: {
+        getCourse: {
+          course_name: 'Course 301',
+          course_description: 'the advanced level - only in person',
+          active: true,
+          virtual_course: false,
+        },
+      },
+    }
+
+    expect(result.data).to.eql(expectedResult)
+  })
+  /* -------------------------- retrieve all courses -------------------------- */
+  it('retrieve all courses', async function () {
+    const result = await testQuery(`#graphql
         query {
   getAllCourses {
     course_name
@@ -79,24 +106,112 @@ describe('Course Resolvers', () => {
   }
 }
         `)
-        const expectedResult = ''
+    const expectedResult = {
+      data: {
+        getAllCourses: [
+          {
+            course_name: 'Course 101',
+            course_description: 'the basics',
+            active: true,
+            virtual_course: true,
+          },
+          {
+            course_name: 'Course 102 - In-Person',
+            course_description: 'the basics, but in person',
+            active: true,
+            virtual_course: false,
+          },
+          {
+            course_name: 'Course 201',
+            course_description: 'the intermediate level',
+            active: true,
+            virtual_course: true,
+          },
+          {
+            course_name: 'Course 301',
+            course_description: 'the advanced level - only in person',
+            active: true,
+            virtual_course: false,
+          },
+          {
+            course_name: 'Course 999',
+            course_description: 'inactive course',
+            active: false,
+            virtual_course: false,
+          },
+        ],
+      },
+    }
 
-        expect(result.data).to.eql(expectedResult)
-    })
-    /* ------------------------------- edit course ------------------------------ */
-    it('edit course', async () => {
-        expect(true).to.be.false
-    })
-    /* -------------- reject update to course -> name already used -------------- */
-    it('reject updating course to a name already registered', async () => {
-        expect(true).to.be.false
-    })
-    /* ------------------------------ delete course ----------------------------- */
-    it('delete course', async () => {
-        expect(true).to.be.false
-    })
-    /* ---------- reject deleting course -> workshops already assigned ---------- */
-    it('reject deleting course when workshops already assigned to it', async () => {
-        expect(true).to.be.false
-    })
+    expect(result.data).to.eql(expectedResult)
+  })
+  /* ------------------------------- edit course ------------------------------ */
+  it('edit course and related workshops', async function () {
+    const result = await testQuery(`#graphql
+mutation {
+  editCourse(uneditedCourseName: "Course 101", courseData: { course_name: "Course 901", course_description: "test", active: true, virtual_course: true}) {
+    course_name
+  }
+  }
+`)
+
+    const expectedResult = {
+      data: {
+        editCourse: {
+          course_name: 'Course 901',
+        },
+      },
+    }
+
+    expect(result.data).to.eql(expectedResult)
+  })
+  /* -------------- reject update to course -> name already used -------------- */
+  it('reject updating course to a name already registered', async function () {
+    const result = await testQuery(`#graphql
+    mutation {
+  editCourse(uneditedCourseName: "Course 999", courseData: { course_name: "Course 101", course_description: "test", active: true, virtual_course: true}) {
+    course_name
+  }
+  }
+    `)
+
+    expect(
+      result.data.errors[0].message.includes(
+        'Unique constraint failed on the fields: (`course_name`)'
+      )
+    ).to.be.true
+  })
+  /* ------------------------------ delete course ----------------------------- */
+  it('delete course', async function () {
+    const result = await testQuery(`#graphql
+          mutation {
+            removeCourse(course_name: "Course 999") {
+              course_name
+            }
+          }
+      `)
+
+    const expectedResult = {
+      data: {
+        removeCourse: {
+          course_name: 'Course 999',
+        },
+      },
+    }
+    expect(result.data).to.eql(expectedResult)
+  })
+  /* ---------- reject deleting course -> workshops already assigned ---------- */
+  it('reject deleting course when workshops already assigned to it', async function () {
+    const result = await testQuery(`#graphql
+          mutation {
+            removeCourse(course_name: "Course 101") {
+              course_name
+            }
+          }
+      `)
+
+    expect(result.data.errors[0].message).to.eql(
+      'Cannot delete course with past or present workshops assigned'
+    )
+  })
 })
