aa21dc09ef8a55c44187292b80a107bc5cae3560
Jeffrey Rose <jtr219@outlook.com>
2021-06-03 12:03:36 -0400

    set up structure of advisor testing

---
+++ tests/resolvers/advisors.test.ts
@@ -0,0 +1,14 @@
+import { describe } from 'mocha'
+//import { expect } from 'chai'
+//import { testQuery } from '../queryTester'
+
+describe('Advisor Resolvers', async function () {
+  it('create advisor')
+  it('reject creating advisor when email (PK) already registered')
+  it('retrieve advisor')
+  it('retrieve all advisors')
+  it('update advisor')
+  it('reject update when new email already registered for other user')
+  it('delete advisor')
+  it('reject deleting advisor when workshops have been scheduled to them')
+})
