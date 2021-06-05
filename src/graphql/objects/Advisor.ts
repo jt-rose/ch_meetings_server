0b1c2eb07d3820e71210d6c28c1219a745452d42
Jeffrey Rose <jtr219@outlook.com>
2021-06-04 23:16:37 -0400

    update column field names

--- src/graphql/objects/Advisor.ts
+++ src/graphql/objects/Advisor.ts
@@ -1,4 +1,7 @@
 import { ObjectType, Field } from 'type-graphql'
+import { AdvisorLanguage } from './Languages'
+import { AdvisorRegion } from './Region'
+import { AdvisorUnavailableDays } from './UnavailableDays'
 
 @ObjectType()
 export class Advisor {
@@ -10,4 +13,13 @@ export class Advisor {
 
   @Field()
   last_name: string
+
+  @Field(() => [AdvisorRegion])
+  regions: AdvisorRegion[]
+
+  @Field(() => [AdvisorLanguage])
+  languages: AdvisorLanguage[]
+
+  @Field(() => [AdvisorUnavailableDays])
+  unavailable_days: AdvisorUnavailableDays[]
 }
