eaaa7399030fdb20aaf3de530e70d1585eee6be4
Jeffrey Rose <jtr219@outlook.com>
2021-06-06 14:13:30 -0400

    add ChangeLog gql object

---
+++ src/graphql/objects/ChangeLog.ts
@@ -0,0 +1,16 @@
+import { ObjectType, Field, Int } from 'type-graphql'
+
+@ObjectType()
+export class ChangeLog {
+  @Field(() => Int)
+  log_id: number
+
+  @Field(() => Int)
+  workshop_id: number
+
+  @Field()
+  note: string
+
+  @Field(() => Date)
+  log_date: Date
+}
