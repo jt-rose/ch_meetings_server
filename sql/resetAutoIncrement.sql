5309ceab2c92d41408b37a9750bc0049e92e20ff
Jeffrey Rose <jtr219@outlook.com>
2021-06-09 01:42:22 -0400

    add additional resets

--- sql/resetAutoIncrement.sql
+++ sql/resetAutoIncrement.sql
@@ -4,3 +4,4 @@
 ALTER SEQUENCE courses_course_id_seq RESTART WITH 6;
 ALTER SEQUENCE clients_client_id_seq RESTART WITH 6;
 ALTER SEQUENCE regions_region_id_seq RESTART WITH 9;
+ALTER SEQUENCE advisor_notes_note_id_seq RESTART WITH 4;
