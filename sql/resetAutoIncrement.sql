3911b038389009061eba08385c1306979542169f
Jeffrey Rose <jtr219@outlook.com>
2021-06-13 23:18:51 -0400

    add advisor_id reset

--- sql/resetAutoIncrement.sql
+++ sql/resetAutoIncrement.sql
@@ -5,3 +5,5 @@ ALTER SEQUENCE courses_course_id_seq RESTART WITH 6;
 ALTER SEQUENCE clients_client_id_seq RESTART WITH 6;
 ALTER SEQUENCE regions_region_id_seq RESTART WITH 9;
 ALTER SEQUENCE advisor_notes_note_id_seq RESTART WITH 4;
+ALTER SEQUENCE unavailable_days_unavailable_id_seq RESTART WITH 8;
+ALTER SEQUENCE advisors_advisor_id_seq RESTART WITH 6;
