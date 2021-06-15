8ad03c8a00d80deb734e06931a5caf9ad1c3ae46
Jeffrey Rose <jtr219@outlook.com>
2021-06-15 01:16:48 -0400

    add language_id reset

--- sql/resetAutoIncrement.sql
+++ sql/resetAutoIncrement.sql
@@ -7,3 +7,4 @@ ALTER SEQUENCE regions_region_id_seq RESTART WITH 9;
 ALTER SEQUENCE advisor_notes_note_id_seq RESTART WITH 4;
 ALTER SEQUENCE unavailable_days_unavailable_id_seq RESTART WITH 8;
 ALTER SEQUENCE advisors_advisor_id_seq RESTART WITH 6;
+ALTER SEQUENCE languages_language_id_seq RESTART WITH 9;
