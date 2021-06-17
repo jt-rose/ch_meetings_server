93a0f528358b1bbed3c10a9e8e1e24d5d54467de
Jeffrey Rose <jtr219@outlook.com>
2021-06-17 12:00:45 -0400

    update session object fields

--- src/graphql/objects/Session.ts
+++ src/graphql/objects/Session.ts
@@ -1,6 +1,7 @@
 import { ObjectType, Field, Int } from 'type-graphql'
-import { RequestedStartTime } from './RequestedStartTime'
+import { RequestedStartTime } from './SessionRequestedStartTime'
 import { SessionNote } from './SessionNote'
+import { SESSION_STATUS } from '../enums/SESSION_STATUS'
 
 @ObjectType()
 export class WorkshopSession {
@@ -10,22 +11,22 @@ export class WorkshopSession {
   @Field()
   workshop_id: number
 
-  @Field(() => Date)
-  session_date: Date
-
   @Field()
-  start_time: string
+  session_name: string
 
-  @Field()
-  session_status: string // enum?
+  @Field(() => Date, { nullable: true })
+  date_and_time: Date
+
+  @Field(() => SESSION_STATUS)
+  session_status: SESSION_STATUS
 
   @Field() // float
   duration_in_hours: number
 
-  @Field()
+  @Field({ nullable: true })
   zoom_link: string
 
-  // workshop field resolver - needed?
+  // field resolvers
 
   @Field(() => [RequestedStartTime])
   requested_start_times: RequestedStartTime[]
