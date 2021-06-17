eea88824eeebfb3ab4690e4d8f65c83ecbe4b7f6
Jeffrey Rose <jtr219@outlook.com>
2021-06-16 23:56:17 -0400

    add coursework object

---
+++ src/graphql/objects/Coursework.ts
@@ -0,0 +1,21 @@
+import { ObjectType, Field, Int } from 'type-graphql'
+import { Course } from './Course'
+
+@ObjectType()
+export class Coursework {
+  @Field(() => Int)
+  coursework_id: number
+
+  @Field()
+  coursework_name: string
+
+  @Field({ nullable: true })
+  coursework_description?: string
+
+  @Field()
+  active: boolean
+
+  @Field(() => [Course])
+  courses: Course[]
+  // workshops field resolver
+}
