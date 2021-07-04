0ca141e8321bbdd74f0a3093d7d3de13380f20e3
Jeffrey Rose <jtr219@outlook.com>
2021-07-03 23:14:09 -0400

    add authChecker to manage auth + roles

---
+++ src/middleware/authChecker.ts
@@ -0,0 +1,28 @@
+import { AuthChecker, Authorized } from 'type-graphql'
+import { Context } from '../utils/context'
+
+// define types of roles
+type Roles = 'ADMIN' // | 'SUPER'
+
+// export descriptive authentication wrappers
+export const Authenticated = () => Authorized()
+export const AdminOnly = () => Authorized('ADMIN')
+
+// authentication / user role logic
+export const authChecker: AuthChecker<Context, Roles> = (
+  { context: { req } },
+  roles
+) => {
+  const { manager_id, admin } = req.session
+
+  // for basic access (no roles specified - @Authorized()),
+  // check that a manager_id has been stored
+  if (roles.length === 0) return !!manager_id
+
+  // for admin access, check that an admin property
+  // has been stored in req
+  if (roles.includes('ADMIN') && admin) return true
+
+  // no roles matched, restrict access
+  return false
+}
