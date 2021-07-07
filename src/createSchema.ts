90ded0500e4f248b1db637f6ee1c7c1da2728540
Jeffrey Rose <jtr219@outlook.com>
2021-07-07 02:29:33 -0400

    remove mock schemas

--- src/createSchema.ts
+++ src/createSchema.ts
@@ -1,4 +1,8 @@
+import 'reflect-metadata'
 import { buildSchema } from 'type-graphql'
+import { authChecker } from './middleware/authChecker'
+
+/* ---------------------------- import resolvers ---------------------------- */
 import { WorkshopSessionResolver } from './graphql/resolvers/SessionResolver'
 import { ClientResolver } from './graphql/resolvers/ClientResolver'
 import { CourseResolver } from './graphql/resolvers/CourseResolver'
@@ -12,23 +16,32 @@ import { ManagerResolver } from './graphql/resolvers/ManagerResolver'
 import { ManagerAssignmentsResolver } from './graphql/resolvers/ManagerAssignmentResolver'
 import { LicenseResolver } from './graphql/resolvers/LicenseResolver'
 
-export const createSchema = () =>
-  buildSchema({
-    resolvers: [
-      WorkshopSessionResolver,
-      ClientResolver,
-      CourseResolver,
-      AdvisorResolver,
-      LanguageResolver,
-      RegionResolver,
-      AdvisorNoteResolver,
-      UnavailableDayResolver,
-      CourseworkResolver,
-      ManagerResolver,
-      ManagerAssignmentsResolver,
-      LicenseResolver,
-    ],
-    validate: false,
-    // automatically create `schema.gql` file with schema definition in project's working directory
-    emitSchemaFile: true,
-  })
+// generates the graphQL schema
+// authChecker is applied as an arg, allowing for DI if needed
+// if a mock schema is set up, make sure to set 'emitSDL' to false
+// to avoid overwriting the real schema SDL
+const generateSchemaType =
+  (customAuthChecker: typeof authChecker, emitSDL: boolean) => () =>
+    buildSchema({
+      resolvers: [
+        WorkshopSessionResolver,
+        ClientResolver,
+        CourseResolver,
+        AdvisorResolver,
+        LanguageResolver,
+        RegionResolver,
+        AdvisorNoteResolver,
+        UnavailableDayResolver,
+        CourseworkResolver,
+        ManagerResolver,
+        ManagerAssignmentsResolver,
+        LicenseResolver,
+      ],
+      validate: false,
+      // automatically create `schema.gql` file with schema definition in project's working directory
+      emitSchemaFile: emitSDL,
+      authChecker: customAuthChecker,
+    })
+
+// create schema for production
+export const createSchema = generateSchemaType(authChecker, true)
