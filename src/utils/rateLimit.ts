fdad5f6ce9eb01067bfcdc30d4fb2c5ce609b0ed
Jeffrey Rose <jtr219@outlook.com>
2021-07-07 02:42:21 -0400

    edit to use updated context parameters

--- src/utils/rateLimit.ts
+++ src/utils/rateLimit.ts
@@ -6,26 +6,25 @@ const ONE_DAY = 60 * 60 * 24
 const standardLimit = 500
 
 type RateLimit = (limit?: number) => MiddlewareFn<Context>
-export const rateLimit: RateLimit = (limit = standardLimit) => async (
-  { context: { req } /*, info */ },
-  next
-) => {
-  // for now, a simple daily user rate limit is sufficient
-  // if we need to rate limit specific queries
-  // we can use the following commented out code
-  //const key = `rate-limit:${info.fieldName}:${req.ip}`
-  const key = `rate-limit:${req.ip}`
+export const rateLimit: RateLimit =
+  (limit = standardLimit) =>
+  async ({ context: { session } /*, info */ }, next) => {
+    // for now, a simple daily user rate limit is sufficient
+    // if we need to rate limit specific queries
+    // we can use the following commented out code
+    //const key = `rate-limit:${info.fieldName}:${req.ip}`
+    const key = `rate-limit:${session.ip}`
 
-  const current = await redis.incr(key)
-  console.log(key, ' is at ', current)
-  if (current > limit) {
-    throw new Error(
-      `You have exceeded the daily rate limit of ${limit} to the server. Please wait until the following day.`
-    )
-  } else if (current === 1) {
-    console.log('expired')
-    await redis.expire(key, ONE_DAY)
+    const current = await redis.incr(key)
+    console.log(key, ' is at ', current)
+    if (current > limit) {
+      throw new Error(
+        `You have exceeded the daily rate limit of ${limit} to the server. Please wait until the following day.`
+      )
+    } else if (current === 1) {
+      console.log('expired')
+      await redis.expire(key, ONE_DAY)
+    }
+
+    return next()
   }
-
-  return next()
-}
