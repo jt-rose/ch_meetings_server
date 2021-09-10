70cab564f082502e6199fcf1ac6dbfae44b17a94
Jeffrey Rose <jtr219@outlook.com>
2021-09-09 23:31:29 -0400

    add SCHEDULING_PROCESS enums

--- src/graphql/enums/WORKSHOP_STATUS.ts
+++ src/graphql/enums/WORKSHOP_STATUS.ts
@@ -14,3 +14,16 @@ registerEnumType(WORKSHOP_STATUS, {
   name: 'WORKSHOP_STATUS',
   description: 'status of workshop requests',
 })
+
+// Coordinators may update the workshop status to VETTING or HOLDING
+// without having to push through a change request
+// this enum is to isolate those two for use in the workshop resolver's
+// "update Scheduing Status" method
+export enum SCHEDULING_PROCESS {
+  VETTING = 'VETTING',
+  HOLDING = 'HOLDING',
+}
+
+registerEnumType(SCHEDULING_PROCESS, {
+  name: 'SCHEDULING_PROCESS',
+})
