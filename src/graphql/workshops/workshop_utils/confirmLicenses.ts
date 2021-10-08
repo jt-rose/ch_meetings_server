78a039833f7f82a0798fc63caf9a53aa7bb34ead
Jeffrey Rose <jtr219@outlook.com>
2021-10-07 23:52:02 -0400

    add branching logic for different license types

--- src/graphql/workshops/workshop_utils/confirmLicenses.ts
+++ src/graphql/workshops/workshop_utils/confirmLicenses.ts
@@ -1,21 +1,33 @@
 import { PrismaClient } from '@prisma/client'
+import { LICENSE_TYPE } from '../../enums/LICENSE_TYPE'
 import { CustomError } from '../../../middleware/errorHandler'
+
+// ! add check for workshop type
+// ! block adding same type twice
 
 export const confirmAvailableLicenses = async (config: {
   prisma: PrismaClient
   client_id: number
   course_id: number
   class_size: number
+  license_type: LICENSE_TYPE
   workshop_id?: number // for editing a workshop request
 }) => {
-  const { prisma, client_id, course_id, class_size, workshop_id } = config
+  const {
+    prisma,
+    client_id,
+    course_id,
+    class_size,
+    license_type,
+    workshop_id,
+  } = config
 
   // find available and reserved licenses of client
   const clientWithLicenses = await prisma.clients.findFirst({
     where: { client_id },
     include: {
       available_licenses: {
-        where: { course_id },
+        where: { course_id, license_type },
         // the reserved license count will be pulled if a workshop id is provided
         // this is used when editing a reserved amount
         // but will default to 0 when checking against a new request
@@ -32,13 +44,29 @@ export const confirmAvailableLicenses = async (config: {
     throw new CustomError('Client is currently inactive!')
 
   // get reserved and remaining amounts, default to 0 when not found
-  const { reserved_amount } =
-    clientWithLicenses.available_licenses[0].reserved_licenses[0] || 0
-  const { remaining_amount } = clientWithLicenses.available_licenses[0] || 0
+  const currentAmountOfReservedLicenses =
+    clientWithLicenses.available_licenses[0].reserved_licenses[0]
+      .reserved_amount || 0
+  const currentAmountOfAvailableLicenses =
+    clientWithLicenses.available_licenses[0].remaining_amount || 0
 
-  // determine remaining licenses after removing class size
+  // throw error if license_type is full workshop but reserved amount for this workshop is greater than one
+  // this error will be hidden from the user but stored in the error log
+  if (license_type === 'FULL_WORKSHOP' && currentAmountOfReservedLicenses > 1) {
+    throw Error(
+      `Full workshop licenses should only have one workshop reservation per workshop reservation id, but workshop #${workshop_id} had ${currentAmountOfReservedLicenses} reservations associated with it`
+    )
+  }
+
+  // map number of licenses needed based on whether the license_type is for individuals or a full_workshop
+  const numberOfLicensesNeeded =
+    license_type === 'FULL_WORKSHOP' ? 1 : class_size
+
+  // determine remaining licenses after removing amount of licenses needed
   const updatedAvailableLicenseAmount =
-    reserved_amount + remaining_amount - class_size
+    currentAmountOfReservedLicenses +
+    currentAmountOfAvailableLicenses -
+    numberOfLicensesNeeded
 
   // reject if too few licenses to accomodate class
   if (updatedAvailableLicenseAmount < 0)
